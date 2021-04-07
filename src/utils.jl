c1ab602b8b51174a8630300730ebc8eff3ac3406
Charis P. Michelakis <ch.p.mxs@gmail.com>
2021-04-07 20:54:34 +0300

    add: utils.jl

---
+++ src/utils.jl
@@ -0,0 +1,28 @@
+""""
+    flexfit(asp::ARCHSpec, data::AbstractVector)
+
+A wrapper around `fit(VS::Type{<:UnivariateVolatilitySpec}, data; dist=StdNormal,
+meanspec=Intercept, algorithm=BFGS(), autodiff=:forward, kwargs...)` that catches
+`AssertionError`s thrown when the requested `ARCHSpec` could not be fitted and falls back to
+fitting GARCH{1,1} as the volatility specification
+"""
+function flexfit(data::AbstractVector, asp::ARCHSpec)
+    try
+        fit(asp.volspec, data, meanspec = asp.meanspec, dist=asp.dist)
+    catch e
+        if isa(e,AssertionError)
+            @warn ("Could not fit $(asp.volspec), falling back to GARCH{1,1}")
+            fit(GARCH{1,1}, data, meanspec = asp.meanspec, dist=asp.dist)
+        else
+            rethrow()
+        end
+    end
+end
+"""
+    dupefit(asp::ARCHSpec, data::AbstractVector; prefitted::Union{ARCHMode[l,Nothing}=nothing])
+
+A wrapper around `flexfit(asp::ARCHSpec, data::AbstractVector)` that fits an `ARCHModel` only once if multiple VaR models depend on the same underlying `ARCHSpec` in order to avoid wasted repetitions
+"""
+function dupefit(asp::ARCHSpec, data::AbstractVector; prefitted::Union{ARCHModel,Nothing}=nothing)
+    prefitted != nothing ? prefitted : flexfit(asp,data)
+end
